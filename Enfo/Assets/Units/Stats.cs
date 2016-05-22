73c8a94c4e0df17f09fc3d3c6d9c2b512e352471
BrittleCornflakes <mossmyr@live.com>
2016-05-22 02:43:27 +0200

    Basic unit stats scrip

---
+++ Enfo/Assets/Units/Stats.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Stats : MonoBehaviour {
+
+    // Fields
+    public float currentHealth;
+    public float maxHealth;
+    public float currentMana;
+    public float maxMana;
+    public float currentExperience;
+    public float currentMovementSpeed;
+
+    // Getters
+    public float CurrentHealth { get { return currentHealth; } }
+    public float MaxHealth { get { return maxHealth; } }
+    public float CurrentMana { get { return currentMana; } }
+    public float MaxMana { get { return maxMana; } }
+    public float CurrentExperience { get { return currentExperience; } }
+    public float CurrentMovementSpeed { get { return currentMovementSpeed; } }
+
+    // Setters
+    void changeHealth(float delta)
+    {
+        currentHealth += delta;
+    }
+
+    void changeMaxHealth(float delta)
+    {
+        maxHealth += delta;
+    }
+
+    void changeMana(float delta)
+    {
+        currentMana += delta;
+    }
+
+    void changeMaxMana(float delta)
+    {
+        maxMana += delta;
+    }
+
+    void changeExperience(float delta)
+    {
+        currentExperience += delta;
+    }
+
+    void changeMovementSpeed(float delta)
+    {
+        currentMovementSpeed += delta;
+    }
+
+
+}
