c36d71d86269c4c266dba077a937547ae2112c0a
Dennis Majvall <dennis.majvall.sp13@thegameassembly.com>
2016-05-22 14:14:22 +0200

    no message

---
+++ Enfo/Assets/Scripts/Effects/StatsChangeEffect.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StatsChangeEffect : MonoBehaviour
+{
+	float Health;
+	float MaxHealth;
+	float HealthRegen;
+
+	float Mana;
+	float MaxMana;
+	float ManaRegen;
+
+	float AttackDamage;
+
+	void Start()
+	{
+
+	}
+	
+	void Update()
+	{
+
+	}
+}
