749de778b98cf29bc21a8d4a5bc746559ed30f95
BrittleCornflakes <mossmyr@live.com>
2016-05-24 00:28:25 +0200

    Added evasion system

--- Enfo/Assets/Scripts/Stats/HeroStatsComponent.cs
+++ Enfo/Assets/Scripts/Stats/HeroStatsComponent.cs
@@ -4,9 +4,10 @@ using System.Collections;
 [System.Serializable]
 public class HeroStats
 {
-	public float experience;
-	public string heroName; // class name, i.e. 'Ranger' 'Paladin'
-	public string properName; // unit name, i.e. 'Sai', 'Oak'
+	public float 	experience;
+	public int 		level;
+	public string 	heroName; // class name, i.e. 'Ranger' 'Paladin'
+	public string 	properName; // unit name, i.e. 'Sai', 'Oak'
 }
 
 public class HeroStatsComponent : UnitStatsComponent
@@ -15,16 +16,19 @@ public class HeroStatsComponent : UnitStatsComponent
 	HeroStats heroStats;
 
 	// Getters
-	public float Experience { get { return heroStats.experience; } }
-	public string HeroName { get { return heroStats.heroName; } }
-	public string ProperName { get { return heroStats.properName; } }
+	public float Experience		{ get { return heroStats.experience; } }
+	public int Level			{ get { return heroStats.level; } }
+	public string HeroName 		{ get { return heroStats.heroName; } }
+	public string ProperName	{ get { return heroStats.properName; } }
 
 
 	// Setters
-	public void ChangeExperience(float delta) { heroStats.experience += delta; }
+	public void ChangeExperience(float delta) 			{ heroStats.experience += delta; }
 
-	public void ChangeHeroName(string newName) { heroStats.heroName = newName; }
+	public void ChangeHeroName(string newName)			{ heroStats.heroName = newName; }
 
-	public void ChangeProperName(string newProperName) { heroStats.properName = newProperName; }
+	public void ChangeProperName(string newProperName) 	{ heroStats.properName = newProperName; }
+
+	public void ChangeLevel(int delta)					{ heroStats.level += delta; }
 
 }
