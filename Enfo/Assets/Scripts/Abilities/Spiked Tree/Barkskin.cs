0771b4473857d639083f2a0c0dff90de8dce1812
SimonMossmyr <mossmyr@live.com>
2016-06-01 21:45:40 +0200

    Started Barkskin ability plus minor fixes * Paladin object in Hierarchy was replaced with Spiked Tree for testing purposes. * UnitTargetableAbility.prefab was moved. * Barbarians attack cooldown time was fixed

---
+++ Enfo/Assets/Scripts/Abilities/Spiked Tree/Barkskin.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Barkskin : Ability
+{
+
+	bool keyWasPressed = false;
+
+	void Start()
+	{
+
+	}
+	
+	void Update()
+	{
+		if (Input.GetKeyDown (KeyCode.F)) {
+			keyWasPressed = true;
+		}
+	}
+}
